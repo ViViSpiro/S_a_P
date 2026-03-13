7f423a13e3587598df99fa1f8a78d42e726148b4
vivispiro <scientiavivispiro@gmail.com>
2026-03-14 00:02:50 +0300

    task_04 done

---
+++ task_04.py
@@ -0,0 +1,39 @@
+# Функция для того, чтобы поменять местами минимальные и максимальные элементы и добавить минимум в конец массива
+def sort_list(lst):
+    # Проверка на пустой список
+    if lst is None or len(lst) == 0:
+        return []
+    
+    # Находим минимальное и максимальное значение
+    min_val = min(lst)
+    max_val = max(lst)
+    
+    # Если минимальное равно максимальному (все элементы одинаковые)
+    if min_val == max_val:
+        return lst + [min_val]
+    
+    # Создаем новый список для результата
+    result = []
+    
+    # Проходим по каждому элементу исходного списка
+    for item in lst:
+        if item == min_val:
+            result.append(max_val)
+        elif item == max_val:
+            result.append(min_val)
+        else:
+            result.append(item)
+    
+    # Добавляем минимальное значение в конец
+    result.append(min_val)
+    
+    return result
+
+
+# Получаем входные данные
+input_str = input()
+try:
+    lst = eval(input_str)
+    print(sort_list(lst))
+except:
+    print([])
